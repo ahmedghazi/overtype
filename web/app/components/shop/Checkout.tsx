a6282c4175cabf709fa00705e32a5c74189d5dda
ahmedghazi <atmet.ghazi@gmail.com>
2025-07-14 19:35:05 +0200

    let s gooo

--- web/app/components/shop/Checkout.tsx
+++ web/app/components/shop/Checkout.tsx
@@ -79,8 +79,8 @@ const BtnCheckout = (props: Props) => {
       settings: {
         displayMode: "overlay",
         theme: "dark",
-        successUrl: "http://localhost:3000/post-checkout?status=success",
-        cancelUrl: "http://localhost:3000/post-checkout?status=canceled",
+        successUrl: "/post-checkout?status=success",
+        cancelUrl: "/post-checkout?status=canceled",
         variant: "multi-page",
       },
     });
