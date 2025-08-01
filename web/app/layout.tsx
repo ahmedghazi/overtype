a360bef2ae988dac39c11bac137fc5c0448232c7
ahmedghazi <atmet.ghazi@gmail.com>
2025-08-01 10:39:11 +0200

    let s gooo

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -13,6 +13,7 @@ import { VisualEditing } from "next-sanity";
 import { ShopWrapper } from "./components/shop/ShopContext";
 import { ToastContainer } from "react-toastify";
 import CookieConsent from "./components/ui/CookieConsent";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
   metadataBase: new URL(website.url),
@@ -72,6 +73,7 @@ export default async function RootLayout({
           </LocaleContextProvider>
         </div>
       </body>
+      <GoogleAnalytics gaId='G-PB2Y9GBKE3' />
     </html>
   );
 }
