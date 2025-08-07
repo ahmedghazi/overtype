a7df3b291ad3eada0ffcfb80ca06aa310d1040ca
ahmedghazi <atmet.ghazi@gmail.com>
2025-08-07 13:13:05 +0200

    let s gooo

--- web/app/api/order-completed/route.ts
+++ web/app/api/order-completed/route.ts
@@ -464,8 +464,12 @@ function generateUserEmailHtml(name: string, order: any, currencyCode: string) {
     if (el && el.fullTitle) {
       const _html = `<div style="background: #ededed; padding: 15px; border-radius: 5px; margin-bottom:10px">
         <h3 style="margin-top:0;color: #666;">${el.fullTitle}</h3>
-        <div style="color: #666;">Use in logo/wordmark: ${el.isLogo}</div>
-        <div style="color: #666;">Size licenses: ${el.license} ${el.licenseInfos}</div>
+        <div style="color: #666;">Use in logo/wordmark: ${
+          el.isLogo === "true" ? "Yes" : "No"
+        }</div>
+        <div style="color: #666;">Size licenses: ${el.license} ${
+        el.licenseInfos
+      }</div>
       </div>`;
       items += _html;
     }
