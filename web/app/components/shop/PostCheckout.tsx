f176b012ad979e09667476ae974585e058cd77ae
ahmedghazi <atmet.ghazi@gmail.com>
2025-08-08 10:39:49 +0200

    let s gooo

--- web/app/components/shop/PostCheckout.tsx
+++ web/app/components/shop/PostCheckout.tsx
@@ -13,7 +13,7 @@ const CheckoutSuccess = () => {
   return (
     <div className='success'>
       <div className='header mb-2xl'>
-        <Logo />
+        {/* <Logo /> */}
         <h1 className='md:text-2xl'>Thank you for your purchase!</h1>
         {/* <p className='md:text-xl'>
           Your download link is on its way to your inbox.
