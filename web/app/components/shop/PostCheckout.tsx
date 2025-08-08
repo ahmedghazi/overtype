90851db119f37f2ab5e51f007c6e6370befdaede
ahmedghazi <atmet.ghazi@gmail.com>
2025-08-08 10:39:34 +0200

    let s gooo

--- web/app/components/shop/PostCheckout.tsx
+++ web/app/components/shop/PostCheckout.tsx
@@ -15,12 +15,20 @@ const CheckoutSuccess = () => {
       <div className='header mb-2xl'>
         <Logo />
         <h1 className='md:text-2xl'>Thank you for your purchase!</h1>
-        <p className='md:text-xl'>
+        {/* <p className='md:text-xl'>
           Your download link is on its way to your inbox.
         </p>
         <p>
           Have fun with your new typeface, <br />
           we can’t wait to see what you do with it.
+        </p> */}
+
+        <p className='md:text-xl'>
+          Your download link is on its way to your inbox.
+        </p>
+        <p>
+          Enjoy your new typeface! <br />
+          We can't wait to see what you create with it.
         </p>
       </div>
       <div className='products flex flex-col gap-2xs'>
