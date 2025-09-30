cd5116e04a90aa5139973e632fe81655c76ee294
ahmedghazi <atmet.ghazi@gmail.com>
2025-09-30 10:20:12 +0200

    let s gooo

--- web/app/components/ui/KeyValString.tsx
+++ web/app/components/ui/KeyValString.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const KeyValStringComponent = ({ input }: Props) => {
   return (
-    <div className='grid grid-cols-5 mb-md'>
+    <div className='grid grid-cols-5 mb-md gap-md'>
       <div className='key text-secondary'>{input.key}</div>
       <div className='val col-span-4'>{input.val}</div>
     </div>
