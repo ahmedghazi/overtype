e1305b55dfcc8e13d09ac26456472e55d95430cf
ahmedghazi <atmet.ghazi@gmail.com>
2025-10-03 11:59:21 +0200

    let s gooo

--- web/app/components/shop/PostCheckout.tsx
+++ web/app/components/shop/PostCheckout.tsx
@@ -3,22 +3,13 @@ import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 import CartItem from "./CartItem";
 import { ProductData } from "@/app/types/extra-types";
-import Logo from "../Logo";
-import useShop from "./ShopContext";
 
 type Props = {};
 
 const CheckoutSuccess = () => {
-  // const { products, setProducts } = useShop();
-
   const raw = localStorage.getItem("products");
   const storedProducts = raw ? JSON.parse(raw) : [];
   const items = storedProducts?.value;
-
-  // useEffect(() => {
-  // localStorage.setItem("overtype-cart", "");
-  // setProducts({ type: "REMOVE_ALL" });
-  // }, [setProducts]);
 
   return (
     <div className='success'>
